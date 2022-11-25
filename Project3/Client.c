ed6c90d0cfcc4cb4092757d7cd8f9f4c81fa0a56
Данил Казанский <danii.kazanskiy@mail.ru>
2022-11-25 15:38:20 +0300

    Работа с клиентом

--- Project3/Client.c
+++ Project3/Client.c
@@ -6,13 +6,41 @@
 int main()
 {
 	system("chcp 1251>nul");
-	LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
+	LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";//им€ канала (такое жеб как и на сервере)
 	HANDLE hNamedPipe;
 	while (TRUE)
 	{
-		hNamedPipe = CreateFile(
-		
+		hNamedPipe = CreateFile(//открываем канал поп аналогии с открытием файла
+			lpszPipeName, GENERIC_READ | GENERIC_WRITE,
+			0, NULL, OPEN_EXISTING, 0, NULL);
+
+		DWORD dwMode = PIPE_READMODE_MESSAGE;
+
+		BOOL isSucces = SetNamedPipeHandleState(
+			hNamedPipe,
+			&dwMode,
+			NULL,
+			NULL
 		);
+		if (!isSucces)
+		{
+			printf("—ервер не отвечает\n");
+		}
+		else
+		{
+			printf("—оединение установлено\n");
+			DWORD size_buffer = SIZE_BUFFER; //размер буффера дл€ записи
+			DWORD actual_written; //сколько на самом деле было записано байт
+
+			char message[SIZE_BUFFER];
+			printf("¬ведите вообщение дл€ сервера:\n");
+			gets(message);
+			LPWSTR buffer = &message; //строкова€ переменна€, значение которой записываетс€ в канал
+			WriteFile(hNamedPipe, buffer, size_buffer,
+				&actual_written, NULL);
+		}
+		Sleep(1000);
+		CloseHandle(hNamedPipe);
 	}
 
 }
